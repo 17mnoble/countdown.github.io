5ad162e6dddc788b82405669c2d4e46ee49ea45a
Mark Noble <17mnoble@gmail.com>
2022-02-23 13:48:55 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+//const currentYear = new Date().getFullYear();
+
+const newCountdown = new Date(`April 21 2022 19:00:00`);
+
+function updateCountdowntime(){
+    const currentTime = new Date();
+    const diff = newCountdown - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60 ) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+    
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+    
+    console.log(s);
+}
+
+setInterval(updateCountdowntime, 1000);
